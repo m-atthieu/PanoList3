c60c8d7e60832d927a1cc4fc72a93807beb1c97a
Matthieu DESILE <matthieu@desile.fr>
2011-09-09 09:06:04 +0200

    Initial Commit

---
+++ PanoList3/AppDelegate.rb
@@ -0,0 +1,15 @@
+#
+#  AppDelegate.rb
+#  PanoList3
+#
+#  Created by Matthieu DESILE on 9/9/11.
+#  Copyright 2011 __MyCompanyName__. All rights reserved.
+#
+
+class AppDelegate
+    attr_accessor :window
+    def applicationDidFinishLaunching(a_notification)
+        # Insert code here to initialize your application
+    end
+end
+
